44d0de3012dae19b926b3f1eb5806f1c7806bff4
AymenShe <ay.alloune@gmail.com>
2026-01-22 12:38:52 +0100

    add generator

--- src/main/scala/ReportGenerator.scala
+++ src/main/scala/ReportGenerator.scala
@@ -23,7 +23,43 @@ object ReportGenerator {
     //   1. Convertir en JSON : report.asJson.spaces2
     //   2. Écrire dans le fichier avec Files.write
     //   3. Gérer les erreurs avec Try
-    val jsonString = report.asJson.spaces2
+    //val jsonString = report.asJson.spaces2
+    
+    val header= """
+    ===============================================
+     RAPPORT D'ANALYSE - FILMS & SÉRIES
+    =============================================== 
+    """
+
+    val total_entry = AnalysisReport.MovieStats.total_movies_parsed
+    val total_valid = AnalysisReport.MovieStats.total_movies_valid
+    val parsing_errors = AnalysisReport.MovieStats.parsing_errors
+    val duplicates_removed = AnalysisReport.MovieStats.duplicates_removed
+
+    val parsing_stats= s"""
+    STATISTIQUES DE PARSING
+    ---------------------------
+    - Entrées totales lues      : {$total_entry}
+    - Entrées valides           : {$total_valid}
+    - Erreurs de parsing        : {$parsing_errors}
+    - Doublons supprimés        : {$duplicates_removed}
+    """
+
+    val format_top_10 = formatTopRated(AnalysisReport.top_10_rated)
+
+    val top_10= s"""
+    TOP 10 - MEILLEURS FILMS
+    ----------------------------
+    """ + format_top_10
+
+
+    val format_top_10_voted = 
+
+    val top_10_voted= s"""
+    TOP 10 - PLUS VOTÉS
+    -----------------------
+    """ 
+    
     val writeAttempt = Try {
       Files.write(Paths.get(filename), jsonString.getBytes(StandardCharsets.UTF_8))
   }
@@ -32,4 +68,15 @@ object ReportGenerator {
       case scala.util.Failure(exception) => Left(s"Erreur d'écriture du fichier: ${exception.getMessage}")
     }
   }
+
+    private def formatTopRated(movies: List[MovieSummary]): String =
+    movies.zipWithIndex
+      .map { case (m, idx) =>
+        f"${idx + 1}%2d. ${m.title}%-22s : ${m.rating}%.1f/10 (${m.votes} votes)"
+      }
+      .mkString("\n")
+
+    private def formatTopVoted(movies: List[MovieSummary]): Stro,g = 
+    movies.zipWithIndex.map
+    {case (m,idx) => f"${idx + 1}%2d. ${m.title} : ${m.rating}%.1f/10 votes"}.mkString("\n")
 }
